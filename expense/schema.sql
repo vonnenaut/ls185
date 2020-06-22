3cd40b0c95c26b37468ff43a7a67a5a19f5444fa
vonnenaut <danielashcom@gmail.com>
2020-06-22 15:17:45 +0700

    added exepnse mini-cli-app with ability to list rows in database

--- expense/schema.sql
+++ expense/schema.sql
@@ -6,9 +6,12 @@ DROP TABLE IF EXISTS expenses;
 
 CREATE TABLE expenses (
   id serial PRIMARY KEY, 
-  amount numeric(6, 2) NOT NULL, 
+  amount numeric(6, 2) NOT NULL CHECK(amount >= 0.01), 
   memo text NOT NULL, 
   created_on date NOT NULL
 );
 
+INSERT INTO expenses (amount, memo, created_on) VALUES (14.56, 'Pencils', NOW());
+INSERT INTO expenses (amount, memo, created_on) VALUES (3.29, 'Coffee', NOW());
+INSERT INTO expenses (amount, memo, created_on) VALUES (49.99, 'Text Editor', NOW());
 
