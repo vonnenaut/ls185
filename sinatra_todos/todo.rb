27653d4da11c0871e4b9a9b310393cc71d0969ff
vonnenaut <danielashcom@gmail.com>
2020-06-25 09:48:45 +0700

    Remove SessionPersistence class from todo.rb and place in separate file, session_persistence.rb and require_relative from todo.rb; copied session_persistence.rb to database_persistence.rb to start process of rewriting class to use PostgreSQL database instead of session

--- sinatra_todos/todo.rb
+++ sinatra_todos/todo.rb
@@ -5,6 +5,8 @@ require "sinatra/content_for"
 require "tilt/erubis"
 require "pry"
 
+require_relative "session_persistence"
+
 configure do
   enable :sessions
   set :session_secret, "secret"
@@ -41,64 +43,6 @@ helpers do
 
     incomplete_todos.each(&block)
     complete_todos.each(&block)
-  end
-end
-
-class SessionPersistence
-  def initialize(session)
-    @session = session
-    @session[:lists] ||= []
-  end
-
-  def find_list(id)
-    @session[:lists].find { |l| l[:id] == id }
-  end
-
-  def all_lists
-    @session[:lists]
-  end
-
-  def create_new_list(list_name)
-    id = next_element_id(@session[:lists])
-    @session[:lists] << { id: id, name: list_name, todos: [] }
-  end
-
-  def delete_list(id)
-    @session[:lists].reject! { |list| list[:id] == id }
-  end
-
-  def update_list_name(id, new_name)
-    list = find_list(id)
-    list[:name] = new_name
-  end
-
-  def create_new_todo(list_id, todo_name)
-    list = find_list(list_id)
-    id = next_element_id(list[:todos])
-    list[:todos] << { id: id, name: todo_name, completed: false }
-  end
-
-  def delete_todo_from_list(list_id, todo_id)
-    list = find_list(list_id)
-    list[:todos].reject! { |todo| todo[:id] == todo_id }
-  end
-
-  def update_todo_status(list_id, todo_id, new_status)
-    list = find_list(list_id)
-    todo = list[:todos].find { |t| t[:id] == todo_id }
-    todo[:completed] = new_status
-  end
-
-  def mark_all_todos_as_completed(list_id)
-    list = find_list(list_id)
-    list[:todos].each { |todo| todo[:completed] = true }
-  end
-
-  private
-
-  def next_element_id(elements)
-    max = elements.map { |todo| todo[:id] }.max || 0
-    max + 1
   end
 end
 
